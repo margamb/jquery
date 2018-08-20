49478f1834295ca43535f980baafa240eb0695b0
Marga Martinez <margamarben@gmail.com>
2018-08-20 16:13:03 +0200

    Add product details and its jquery

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 $(document).ready(() => {
+
+    $('.poster-details').hide();
+
+    $(".more-details-button").on('click', event => {
+        $(".poster-details").toggle(200)
+    });
+    
+    /*$(".more-details-button").on("click", event => {
+        $(event.target).toggle(".active")
+    })*/
+
+    $('.more-details-button').on('click', event => {
+        $(event.currentTarget).toggleClass('active');
+    });
+
+    
+
+    $('.poster-details li').on('click', event => {
+        $(event.currentTarget).addClass('active-size');
+        
+        $(event.currentTarget).siblings().removeClass('active-size')
+        
+        $(event.currentTarget).closest('.poster-details').children().removeClass('disabled')
+        
+        
+      });
+    
+    
+
+    //////////////////////////////////////////////////
     
     $(".hide-button").on("click", () => {
         $(".ingrid").hide();
@@ -14,15 +44,15 @@ $(document).ready(() => {
 
 
     $(".fade-out-button").on("click", () => {
-        $(".ingrid1").fadeOut(500);
+        $(".christopherLee").fadeOut(500);
     });
 
     $(".fade-in-button").on("click", () => {
-        $(".ingrid1").fadeIn(4000);
+        $(".christopherLee").fadeIn(4000);
     });
 
     $(".fade-toggle-button").on("click", () => {
-        $(".ingrid1").fadeToggle();
+        $(".christopherLee").fadeToggle();
     });
 
 
